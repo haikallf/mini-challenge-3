7a79484c4817b8d1c58266a1e3c21a20ae4e6170
haikallf <haikallzrd@gmail.com>
2023-08-21 16:41:13 +0700

    initial commit

---
+++ G-Eco Clips/G_Eco_ClipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  G_Eco_ClipsApp.swift
+//  G-Eco Clips
+//
+//  Created by Haikal Lazuardi Fadil on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct G_Eco_ClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ G-Eco/G_EcoApp.swift
@@ -0,0 +1,17 @@
+//
+//  G_EcoApp.swift
+//  G-Eco
+//
+//  Created by Haikal Lazuardi Fadil on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct G_EcoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
